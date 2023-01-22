305390db22bc1bda84aafd5004bf72d4eab4ecbc
VortexOle <93073888+VortexOle@users.noreply.github.com>
2023-01-22 12:49:18 +0100

    register page

---
+++ register/js/pswchecker.js
@@ -0,0 +1,7 @@
+"use strict";
+
+let firstpsw = document.getElementbyId('password-field').value
+let secondpsw = document.getElementbyId('repeat-password').value
+
+console.log(firstpsw)
+console.log(secondpsws)
