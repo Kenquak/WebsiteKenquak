64e2a632de19f482fd804f22715eccd0b6c63673
VortexOle <93073888+VortexOle@users.noreply.github.com>
2023-01-29 15:33:46 +0100

    added php

---
+++ backend/Insert_to_db.php
@@ -0,0 +1,89 @@
+<?php
+/***
+Display the data keys and values for debugging purposes.
+***/
+echo '<pre>', print_r($_POST, 1) , '</pre>';
+
+/***
+Test the data if it's a valid submission by checking the submission ID.
+***/
+if (!isset($_POST['submission_id'])) {
+	die("Invalid submission data!");
+}
+/***
+## Database Config
+
+NOTE: 
+Replace the values below with your MYSQL database environment variables 
+to create a valid connection.
+***/
+$db_host = "your_db_host";
+$db_username = "your_db_username";
+$db_password = "your_db_password";
+$db_name = "your_database";
+$db_table = "your_table";
+
+// Taking all 5 values from the form data(input)
+
+/***
+Connect to database.
+***/
+$mysqli = new mysqli($db_host, $db_username, $db_password, $db_name);
+if ($mysqli->connect_error) {
+	die("Connection failed: " . $mysqli->connect_error);
+}
+/***
+## Data to Save
+
+Prepare the data to prevent possible SQL injection vulnerabilities to the database.
+
+NOTE: Add the POST data to save in your database.
+To view the submission as POST data, see: https://www.jotform.com/help/?p=607527
+***/
+$name = $mysqli->real_escape_string(implode(" ", $_POST['username']));
+$email = $mysqli->real_escape_string($_POST['email']);
+$password = $mysqli->real_escape_string($_POST['password']);
+
+/***
+Prepare the test to check if the submission already exists in your database.
+***/
+$sid = $mysqli->real_escape_string($_POST['submission_id']);
+$result = $mysqli->query("SELECT * FROM $db_table WHERE submission_id = '$sid'");
+
+
+if ($result->num_rows > 0) {
+	/* UPDATE query */
+	$result = $mysqli->query("UPDATE $db_table 
+		SET name = '$name',
+			email = '$email', 
+			message = '$message' 
+		
+		WHERE submission_id = '$sid'
+	");
+}
+else {
+	/* INSERT query */
+	$result = $mysqli->query("INSERT IGNORE INTO $db_table (
+		submission_id, 
+		username,
+		email, 
+		password
+	) VALUES (
+		'$sid', 
+		'$name', 
+		'$email',
+		'$password')
+	");
+}
+/***
+Display the outcome.
+***/
+if ($result === true) {
+	echo "Success!";
+}
+else {
+	echo "SQL error:" . $mysqli->error;
+}
+
+$mysqli->close();
+
